55572ab1f0abcb6f4ca88a328030d38299adcdc2
Slava Ovchinnikov <vo@nextop.inc>
2025-05-05 16:18:55 +0400

    fix: failed build message

--- src/bitbucket/index.ts
+++ src/bitbucket/index.ts
@@ -2,9 +2,10 @@ import dedent from "dedent";
 import { Body } from "./types";
 
 export const getMessage = (event: Body) => {
+  console.log(JSON.stringify(event, null, 2));
 
   if('commit_status' in event && event.commit_status.state === 'FAILED') {
-    return dedent`🔴 ${event.commit_status.repository.name} build failed. <a href="${event.commit_status.url}">View details</a>`;
+    return dedent`🔴 ${event.commit_status.repository.name} build <a href="${event.commit_status.url}">failed</a>`;
   }
 
   return undefined
